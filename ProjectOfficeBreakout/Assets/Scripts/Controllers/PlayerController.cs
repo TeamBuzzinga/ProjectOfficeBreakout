99e33d5fede66ba94c90e5660d5bb9b97c534fa4
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-09-26 20:17:24 -0400

    Walk Mechanics and Camera Mechanics added

---
+++ ProjectOfficeBreakout/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    float horizontalInput;
+	float verticalInput;
+	WalkMechanics walkMechanics;
+
+	void Update() {
+		horizontalInput = Input.GetAxisRaw ("Horizontal");
+		verticalInput = Input.GetAxisRaw ("Vertical");
+
+		walkMechanics.setVerticalInput (verticalInput);
+		walkMechanics.setHorizontalInput (horizontalInput);
+		
+	}
+
+	void Start() {
+		walkMechanics = GetComponent<WalkMechanics> ();
+	}
+
+}

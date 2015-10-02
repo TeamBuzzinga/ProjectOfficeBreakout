dcd0bd5f3723964c3eb42cea521137aa753837bd
mjc9732 <mjc9732@gmail.com>
2015-10-02 17:28:55 -0400

    Winning Condtion
    Winning Condition by collision. Check DavidChagDemo.scene

---
+++ ProjectOfficeBreakout/Assets/Scripts/GameMechanics/CollisionDetector.js
@@ -0,0 +1,18 @@
+﻿var toggleGUI : boolean;
+var TurnImage : Texture2D;
+ 
+function OnTriggerEnter (other : Collider)
+{
+        toggleGUI = true;
+}
+ 
+function OnTriggerExit (other : Collider)
+{
+        toggleGUI = false;
+}
+ 
+function OnGUI ()
+{
+        if (toggleGUI == true)
+        GUI.DrawTexture(Rect(Screen.width / 2-150, 0,300,300), TurnImage);
+}
